f47a0ca1dbdba39bb156826748956d0d2b04e728
cjp0421 <76797183+cjp0421@users.noreply.github.com>
2024-06-19 17:45:11 -0500

    built out general layout for top half of paghe

--- src/App.tsx
+++ src/App.tsx
@@ -7,15 +7,37 @@ function App() {
   return (
     <>
       <header>
-
+        <div className='header'><a href='#'>Github</a>|<a href='#'>Portfolio</a>|<a href='#'>LinkedIn</a></div>
+        <h1>Jammming</h1>
       </header>
 
       <main>
+        <div>
+          <input type='text' />
+          <br />
+          <button>Search by Title</button>
+        </div>
+
+        <div>
+          search results
+          - track list
+          - track
+        </div>
+        <div>
+          playlist
+          - playlist name text box / display
+          - tracklist
+          - track
+        </div>
+
 
       </main>
 
       <footer>
-
+        <div className='footer'>
+          This will go to GitHub ReadMe
+          <a href='#'>About</a>
+        </div>
       </footer>
     </>
   )
