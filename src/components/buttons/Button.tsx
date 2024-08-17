1b4ab3f4e5b15c27daa898490ff65fd065e96ebd
cjp0421 <76797183+cjp0421@users.noreply.github.com>
2024-08-17 16:28:12 -0500

    set up button component and css outline

---
+++ src/components/buttons/Button.tsx
@@ -0,0 +1,9 @@
+function Button() {
+    return (
+        <div className="Button">
+            {/* <button class="Track-action"><!-- + or - will go here --></button> */}
+        </div>
+    );
+}
+
+export default Button;
