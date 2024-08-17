5515cff7698cc62a79b5930444ac1d2b8033b284
cjp0421 <76797183+cjp0421@users.noreply.github.com>
2024-08-17 16:52:38 -0500

    added Bootstrap, cleaned up App css

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App/App.tsx'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './index.module.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
