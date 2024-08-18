50fc3ab2f76847e057a7a8168d7b3d8f6f3a1315
cjp0421 <76797183+cjp0421@users.noreply.github.com>
2024-08-18 18:59:14 -0500

    Added media query for header links to make it more responsive

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -6,11 +6,26 @@ function App() {
     <div className={styles.App}>
       <header className='header'>
         <div className='row'>
-          <h1 className='col-5'> Ja<span className={styles.highlight}>mmm</span>ing</h1>
-          <div className='col-7 text-end'>
-            <a target="_blank" referrerPolicy="no-referrer"
-              href='https://github.com/cjp0421/jammming2024'>
-              Github</a><a href='#'>Portfolio</a><a target="_blank" referrerPolicy="no-referrer" href='https://www.linkedin.com/in/carol-joy-pedersen'>LinkedIn</a>
+          <div className='col-6'>
+
+            <h1> Ja<span className={styles.highlight}>mmm</span>ing</h1>
+          </div>
+          <div className='col-6 text-end'>
+            <div className={styles["header-links"]}>
+              <a
+                className={styles["header-link"]}
+                target="_blank"
+                referrerPolicy="no-referrer"
+                href='https://github.com/cjp0421/jammming2024'>
+                Github</a>
+              <a
+                className={styles["header-link"]}
+                href='#'>Portfolio</a>
+              <a
+                className={styles["header-link"]}
+                target="_blank" referrerPolicy="no-referrer" href='https://www.linkedin.com/in/carol-joy-pedersen'>LinkedIn</a>
+            </div>
+
           </div>
         </div>
       </header>
