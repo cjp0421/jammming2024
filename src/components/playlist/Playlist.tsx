73b4e308dc7ae2114b8bb4e5b7bb216d588cfcb5
cjp0421 <76797183+cjp0421@users.noreply.github.com>
2024-09-14 18:36:37 -0500

    began adding types to Playlist, added expected properties to Tracklist

--- src/components/playlist/Playlist.tsx
+++ src/components/playlist/Playlist.tsx
@@ -1,12 +1,12 @@
 import Tracklist from "../tracklist/Tracklist";
 import styles from "./Playlist.module.css"
 
-function Playlist(props) {
+function Playlist(props: { playlistTracks: any[]; isRemoval: any; playlistName: any; onAdd: any; onRemove: any; }) {
     return (
         <div className={styles.Playlist}>
             <input defaultValue={"New Playlist"} />
             {/* <!-- Add a TrackList component --> */}
-            <Tracklist userSearchResults={props.playlistTracks} />
+            <Tracklist userSearchResults={props.playlistTracks} isRemoval={props.isRemoval} onAdd={props.onAdd} onRemove={props.onRemove} />
             <button className={styles["Playlist-save"]}>
                 SAVE TO SPOTIFY
             </button>
