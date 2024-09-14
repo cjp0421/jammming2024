22a3048be7a40bb9d32170d3f1edc6fc027d3ded
cjp0421 <76797183+cjp0421@users.noreply.github.com>
2024-09-14 18:37:45 -0500

    added state to SearchBar, began fixing UI

--- src/components/searchbar/SearchBar.tsx
+++ src/components/searchbar/SearchBar.tsx
@@ -1,19 +1,29 @@
 import styles from "./Searchbar.module.css"
+import { useState } from "react"
 
 function SearchBar() {
+    const [term, setTerm] = useState("")
+
+    const handleTermChange = ({ target }) => {
+        setTerm(target.value)
+    }
+
     return (
         <div className={styles.SearchBar}>
-            <div className='search'>
+            {/* <div className='search'>
                 <input type='text' />
                 <br />
                 <button className='button'>Search by Title</button>
-            </div>
+            </div> */}
+
+
 
             <input
                 placeholder="Enter A Song, Album, or Artist"
+                onChange={handleTermChange}
             />
 
-            <button className={styles.SearchButton} >
+            <button className={styles.SearchButton}>
                 SEARCH
             </button>
 
