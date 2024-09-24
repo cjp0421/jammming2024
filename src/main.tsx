2aa78bfafd76182506b7ab3552e07b781ac2bbe8
cjp0421 <76797183+cjp0421@users.noreply.github.com>
2024-09-24 12:40:50 -0500

    removed index css and consolidated it with app css

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App/App.tsx'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import './index.module.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
