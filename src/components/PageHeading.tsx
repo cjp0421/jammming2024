2c51925b680f1527f8d5e9c21ead2f0a62d14c05
cjp0421 <76797183+cjp0421@users.noreply.github.com>
2025-12-08 10:01:04 -0600

    revamped header section to use basic mui styles

--- src/components/PageHeading.tsx
+++ src/components/PageHeading.tsx
@@ -1,40 +1,69 @@
-import { Typography } from "@mui/material";
+import { AppBar, Grid, Link, Toolbar, Typography } from "@mui/material";
 
 export default function PageHeading() {
     return (
         <header className="page-heading-container">
 
-            <Typography variant="h1" className="page-title">
-                Ja<span className="highlight">mmm</span>ing
-            </Typography>
-
-            <nav className="header-links">
-                <a
-                    className="header-link"
-                    target="_blank"
-                    referrerPolicy="no-referrer"
-                    href="https://github.com/cjp0421/jammming2024"
-                >
-                    Github
-                </a>
-
-                <a
-                    className="header-link"
-                    target="_blank"
-                    href="https://cjp0421.github.io/my-portfolio/"
-                >
-                    Portfolio
-                </a>
-
-                <a
-                    className="header-link"
-                    target="_blank"
-                    referrerPolicy="no-referrer"
-                    href="https://www.linkedin.com/in/carol-joy-pedersen"
-                >
-                    LinkedIn
-                </a>
-            </nav>
+            <AppBar
+                component="nav"
+                sx={{
+                    backgroundColor: '#431448',
+                }}
+            >
+                <Toolbar>
+                    <Typography
+                        variant="h1"
+                        sx={{
+                            fontSize: '3rem',
+                            flexGrow: 1,
+                            color: '#fff'
+                        }}>
+                        Ja<span className="highlight">mmm</span>ing
+                    </Typography>
+                    <Grid
+                        container
+                        sx={{
+                            gap: 5
+                        }}
+                    >
+                        <Link
+                            className="header-link"
+                            target="_blank"
+                            referrerPolicy="no-referrer"
+                            href="https://github.com/cjp0421/jammming2024"
+                            sx={{
+                                color: '#fff',
+                                textDecoration: 'none'
+                            }}
+                        >
+                            Github
+                        </Link>
+                        <Link
+                            className="header-link"
+                            target="_blank"
+                            href="https://cjp0421.github.io/my-portfolio/"
+                            sx={{
+                                color: '#fff',
+                                textDecoration: 'none',
+                            }}
+                        >
+                            Portfolio
+                        </Link>
+                        <Link
+                            className="header-link"
+                            target="_blank"
+                            referrerPolicy="no-referrer"
+                            href="https://www.linkedin.com/in/carol-joy-pedersen"
+                            sx={{
+                                color: '#fff',
+                                textDecoration: 'none'
+                            }}
+                        >
+                            LinkedIn
+                        </Link>
+                    </Grid>
+                </Toolbar>
+            </AppBar>
 
         </header>
     );
