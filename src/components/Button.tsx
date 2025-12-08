3accd6438a0710c520562e110a7a15b6801257e9
cjp0421 <76797183+cjp0421@users.noreply.github.com>
2025-12-08 12:47:22 -0600

    deleted unused button component

--- src/components/Button.tsx
+++
@@ -1,9 +0,0 @@
-function Button() {
-    return (
-        <div>
-            {/* <button class="Track-action"><!-- + or - will go here --></button> */}
-        </div>
-    );
-}
-
-export default Button;
